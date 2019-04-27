8d269e23521bae25a42f2339232750e0f5d94a9a
Ch <47299816+cdsProjects@users.noreply.github.com>
2019-04-27 09:39:44 +0200

    Add files via upload

---
+++ appruebas.js
@@ -0,0 +1,34 @@
+$(function () {
+	// Petición para obtener fotos públicas de los contactos
+	var peticion = 'https://api.flickr.com/services/rest/?&method=flickr.photos.getContactsPublicPhotos&api_key=' 
+		+ api_key + '&user_id=' +user_id + '&extras=date_upload%2C+date_taken&format=json&nojsoncallback=1';
+	var items = [];
+
+	$.when($.getJSON(peticion, function obtener_fotos(info) { // Obtenemos las fotos públicas de los contactos
+		var i;
+		for (i=0;i<info.photos.photo.length;i++) {
+			var item = {url: "", owner: "", name: "", date_taken: "", date_upload: ""}; // Estructura de datos
+			var aux = info.photos.photo[i]; // Obtenemos una a una
+			item.url = 'https://farm'+aux.farm+".staticflickr.com/"+aux.server +'/'+aux.id+'_'+aux.secret+'_m.jpg'; // URL
+			item.owner = aux.owner; // Identificador de usuario
+			item.date_taken = aux.datetaken; // Fecha de la foto
+			item.date_upload = aux.dateupload; // Fecha de subida
+			items.push(item); // Guardamos en lista de ítems
+		}
+	})).then(function obtener_nombres() { // Obtenemos nombres reales de cada usuario
+		for (i=0;i<items.length;i++) { // Para cada item
+			var item = items[i];
+			peticion = 'https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key='
+				+ api_key + '&user_id=' + item.owner + '&format=json&nojsoncallback=1';
+			peticion_nombre(peticion, item); // Una petición para información de usuario
+		}
+	});
+	console.log(items); // Mostramos por consola
+})
+
+function peticion_nombre(peticion, item) { // Función para obtener nombre real a partir de nombre usuario
+	$.getJSON(peticion, function obtener_nombre(datos) { // JSON con información del usuario
+		real  = datos.person.realname._content; // Obtenemos nombre real
+		item.name = real; // Añadimos al ítem
+	});
+}	
