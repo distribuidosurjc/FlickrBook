122d12fefcdedfcb5abb3cfbd0a7522584704463
sergiopg97 <sergiopalaciosgil@hotmail.es>
2019-04-27 14:20:32 +0200

    Miniatura añadida

--- appruebas.js
+++ appruebas.js
@@ -4,12 +4,12 @@ $(function () {
 		+ api_key + '&user_id=' +user_id + '&extras=date_upload%2C+date_taken&format=json&nojsoncallback=1';
 	var items = [];
 
-	$.when($.getJSON(peticion, function obtener_fotos(info) { // Obtenemos las fotos públicas de los contactos
+	$.when($.getJSON(peticion, mostrar_fotos, function obtener_fotos(info) { // Obtenemos las fotos públicas de los contactos
 		var i;
 		for (i=0;i<info.photos.photo.length;i++) {
 			var item = {url: "", owner: "", name: "", date_taken: "", date_upload: ""}; // Estructura de datos
 			var aux = info.photos.photo[i]; // Obtenemos una a una
-			item.url = 'https://farm'+aux.farm+".staticflickr.com/"+aux.server +'/'+aux.id+'_'+aux.secret+'_m.jpg'; // URL
+			item.url = 'https://farm'+aux.farm+".staticflickr.com/"+aux.server +'/'+aux.id+'_'+aux.secret+'_t.jpg'; // URL
 			item.owner = aux.owner; // Identificador de usuario
 			item.date_taken = aux.datetaken; // Fecha de la foto
 			item.date_upload = aux.dateupload; // Fecha de subida
@@ -25,6 +25,20 @@ $(function () {
 	});
 	console.log(items); // Mostramos por consola
 })
+
+function mostrar_fotos(info){
+	$("#imagenes #img").remove(); 
+	var i;
+	for (i=0;i<info.photos.photo.length;i++) {
+		var item = info.photos.photo[i];
+		var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
+		+'/'+item.id+'_'+item.secret+'_t.jpg';
+		console.debug(url);       
+		$("#imagenes").append($("<img id=\"img\" />").attr("src",url));                         
+	}
+	//$("#imagenes #img").click(zoom);
+}
+
 
 function peticion_nombre(peticion, item) { // Función para obtener nombre real a partir de nombre usuario
 	$.getJSON(peticion, function obtener_nombre(datos) { // JSON con información del usuario
